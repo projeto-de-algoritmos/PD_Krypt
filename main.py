a5635565e82f6a8b0cff37b18405555ec2a902cf
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-12-04 17:16:45 -0300

    Adiciona template home_page

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from waitress import serve
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def home_page():
+    return render_template("home_page.html")
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)
