83a9051489de92d6316d378032d39556c7572d86
João Rodrigues <rodrigues.jmatheus@gmail.com>
2020-12-04 20:57:51 -0300

    Inicia página resultados

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from waitress import serve
 
 app = Flask(__name__)
@@ -6,7 +6,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def home_page():
+    text = request.form.get("text")
+    if text:
+        return redirect(url_for('result', text=text))
+
     return render_template("home_page.html")
+
+
+@app.route('/result/<text>', methods=["GET", "POST"])
+def result(text):
+    return render_template("result.html", data=text)
 
 
 if __name__ == '__main__':
